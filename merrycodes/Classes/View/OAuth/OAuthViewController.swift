926186f54472be87a3a231d41be955d06e75f5b0
merrycodes <merrycodes@163.com>
2020-05-21 16:37:35 +0800

    :tada:

--- merrycodes/Classes/View/OAuth/OAuthViewController.swift
+++ merrycodes/Classes/View/OAuth/OAuthViewController.swift
@@ -9,12 +9,6 @@ class OAuthViewController: UIViewController {
         
         webView.loadRequest(URLRequest(url: NetworkTools.sharedTools.OAuthURL as URL))
     }
-    
-    override func loadView() {
-        view = webView
-        title = "登陆新浪微博"
-        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "关闭", style: .plain, target: self, action: #selector(OAuthViewController.close))
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,6 +16,36 @@ class OAuthViewController: UIViewController {
     
     @objc public func close(){
         dismiss(animated: true, completion: nil)
+    }
+    
+    @objc public func autoFill(){
+        var js = "document.getElementById('userId').value='13542285068';"
+        js+="document.getElementById('passwd').value='lutao.158';"
+        webView.stringByEvaluatingJavaScript(from: js)
+    }
+    
+}
+extension OAuthViewController:UIWebViewDelegate{
+    
+    func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        guard let url = request.url , url.host == "www.baidu.com" else{
+            return true
+        }
+        guard let query = url.query , query.hasPrefix("code=") else {
+            print("取消授权")
+            return false
+        }
+        let code = String(query["code=".endIndex...])
+        print(code)
+        return false
+    }
+    
+    override func loadView() {
+        view = webView
+        webView.delegate = self
+        title = "登陆新浪微博"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "关闭", style: .plain, target: self, action: #selector(OAuthViewController.close))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "自动填充", style: .plain, target: self, action: #selector(OAuthViewController.autoFill))
     }
     
 }
